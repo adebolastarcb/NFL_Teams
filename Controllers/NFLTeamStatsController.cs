dc974400decd7fb6ee5c0cc1b47a643cdc1a3632
AdebolaIbiyode <AdebolaIbiyode@LON-LPT-26>
2024-01-31 23:46:36 +0000

    Add project files.

---
+++ Controllers/NFLTeamStatsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NFL_Teams.DataObject;
+
+
+namespace NFL_Teams.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NFLTeamStatsController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public NFLTeamStatsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("matchupstats/{teamName}")]
+        public async Task<ActionResult<MatchUpStats>> GetMatchUpStats(int teamName)
+        {
+            string requestUri = $"https://sports.snoozle.net/search/nfl/searchHandler?fileType=inline&statType=teamStats&season=2020&teamName={teamName}";
+
+            try
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                HttpResponseMessage response = await httpClient.GetAsync(requestUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    MatchUpStats matchUpStats = JsonConvert.DeserializeObject<MatchUpStats>(json);
+                    return Ok(matchUpStats);
+                }
+                return NotFound();
+            }
+            catch (HttpRequestException e)
+            {
+                // Log the exception (implementation depends on the logging framework being used)
+                return StatusCode(500, "Error accessing the NFL stats service");
+            }
+        }
+
+    }
+}
---
+++ DataObject/NFLTeamStats.cs
@@ -0,0 +1,43 @@
+﻿namespace NFL_Teams.DataObject
+{
+
+    public class MatchUpStats
+    {
+        public List<NFLTeamStats>? matchUpStats { get; set; }
+    }
+    public class NFLTeamStats
+    {
+        public required string neutral { get; set; } = "false";
+        public required string visTeamName { get; set; }
+        public required GameStats visStats { get; set; }
+        public required string homeTeamName { get; set; }
+        public required GameStats homeStats { get; set; }
+        public required string isFinal { get; set; } = "false";
+        public required DateTime date { get; set; } = DateTime.MinValue;
+    }
+
+    public class GameStats
+    {
+        public required string StatIdCode { get; set; }
+        public required string GameCode { get; set; }
+        public int TeamCode { get; set; }
+        public required string GameDate { get; set; }
+        public int RushYds { get; set; }
+        public int RushAtt { get; set; }
+        public int PassYds { get; set; }
+        public int PassAtt { get; set; }
+        public int PassComp { get; set; }
+        public int Penalties { get; set; }
+        public int PenaltYds { get; set; }
+        public int FumblesLost { get; set; }
+        public int InterceptionsThrown { get; set; }
+        public int FirstDowns { get; set; }
+        public int ThridDownAtt { get; set; }
+        public int ThirdDownConver { get; set; }
+        public int FourthDownAtt { get; set; }
+        public int FourthDownConver { get; set; }
+        public int TimePoss { get; set; }
+        public int Score { get; set; }
+    }
+
+}
